f3fdb259aaddb7777aed2d6dc0e7c8e9e3a19330
Sinué Garcia Jacobo <sinux@tesla.lan>
2011-03-03 16:05:41 -0600

    Commit Inagural HHub Reward Points

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HHRP::Application.config.secret_token = 'e72dd3ae9c99f87e88570fd95a1be9600e5ccf91f5ddc14392842c34348c1e5ada41711c2677ece061946806e68b267d0abe456bb18ebd3d6a7027bab0fec8f6'
